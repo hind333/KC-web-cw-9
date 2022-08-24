c3ad38bc70d4500dea17d1428510b2eca80201a0
hind333 <109863744+hind333@users.noreply.github.com>
2022-08-24 19:45:38 +0300

    done

--- script.js
+++ script.js
@@ -1,23 +1,24 @@
 // أنشئ المصفوفة تحت هذا الكومنت
+let orders = [];
 
-
-function addOrder() {
+function addOrder(){
   let name = document.getElementById("name").value;
   let id = document.getElementById("id").value;
   let phone = document.getElementById("phone").value;
   let order = document.getElementById("order").value;
   
   // أنشئ كائن newOrder
-  
+  let newOrder ={name,id,phone,order};
   // أدرج الكائن إلى المصفوفة
-  
+  orders.push(newOrder)
   // طبق الخطوة 7 تحت هذا الكومنت
-  
+  load()
 }
 
-function load() {
+function load(){
   let container = document.getElementById("container");
-  container.innerHTML = "";
+  // container.innerHTML = "";
   // طبّق الخطوة 6 تحت هذا الكومنت
-
-}
+orders.forEach(item => {
+  container.innerHTML = `<div><h1>${item.name} - ${item.id}</h1><h3>${item.order}</h3></div>`
+})}
